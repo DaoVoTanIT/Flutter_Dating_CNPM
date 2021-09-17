b52d4da09b030c0715aaae2d94b2c01d7ec4a884
DaoVoTanIT <daovotan3@gmail.com>
2021-09-17 23:40:18 +0700

    initial Route is splashPage

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       ),
       //set up router
 
-      initialRoute: RouteName.homePage,
+      initialRoute: RouteName.splashPage,
       onGenerateRoute: Routers.generateRoute,
     );
   }
