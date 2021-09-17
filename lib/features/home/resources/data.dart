874aa3194c36e99fce30391954b730fbe65f2bb7
DaoVoTanIT <daovotan3@gmail.com>
2021-09-17 22:24:12 +0700

    change data

--- lib/features/home/resources/data.dart
+++ lib/features/home/resources/data.dart
@@ -1,72 +1,6 @@
 import 'package:flutter_dating_cdcnpm/features/home/model/userInfor.dart';
 import 'package:flutter_dating_cdcnpm/features/signin/model/userInfor.dart';
 
-//List<UserInfo> peoples = [
-// final peoples = [
-//   UserInfo(
-//       "0",
-//       "Adam Livene Adam Livene Adam Livene Adam Livene",
-//       'https://c1.wallpaperflare.com/preview/575/637/231/fashion-style-boys-fashion-pose.jpg',
-//       21,
-//       'Software Developer',
-//       '1.000km'),
-//   UserInfo(
-//       "1",
-//       "Derek Staham",
-//       'https://c0.wallpaperflare.com/preview/387/236/63/fashion-style-boys-fashion-pose.jpg',
-//       25,
-//       'Engineer in Mechatronics',
-//       '800m'),
-//   UserInfo(
-//       "2",
-//       "Alexa Georigna",
-//       'https://c0.wallpaperflare.com/preview/432/701/139/fashion-style-boys-fashion-pose.jpg',
-//       23,
-//       'Photographer 📷',
-//       '500m'),
-//   UserInfo(
-//       "3",
-//       "Maxii",
-//       'https://thumbs.dreamstime.com/z/hot-girl-bikini-sunset-19493901.jpg',
-//       23,
-//       'Camerographer 📷',
-//       '2km'),
-//   UserInfo(
-//       '4',
-//       "Risica Nibah",
-//       'https://thumbs.dreamstime.com/z/hot-girl-posing-young-short-dress-sitting-tires-next-to-retro-car-43468453.jpg',
-//       26,
-//       'Studying in W.A Eng.',
-//       '900m'),
-//   UserInfo(
-//       '5',
-//       "Christina",
-//       'https://thumbs.dreamstime.com/z/hot-young-beautiful-girl-posing-beach-sunset-20941835.jpg',
-//       34,
-//       'Developer Advocate 👔',
-//       '10km'),
-//   UserInfo(
-//       '6',
-//       "Rissu Stelin",
-//       'https://thumbs.dreamstime.com/z/hot-asian-girl-dancing-11847386.jpg',
-//       23,
-//       'Studying Aerospace 🛫',
-//       '200m'),
-//   UserInfo(
-//       '7',
-//       "Rebicca",
-//       'https://thumbs.dreamstime.com/z/hot-brunette-girl-hot-sunlight-19477454.jpg',
-//       24,
-//       'MIT Open Courseware 📚',
-//       '50km'),
-// ];
-// this.name,
-//     this.designation,
-//     this.mutualFriends,
-//     this.age,
-//     this.bio,
-//     this.imgUrl,
-//     this.location,
 final dummyUsers = [
   User(
       name: 'Lauren Turner',
@@ -160,7 +94,7 @@ final dummyUsers = [
     age: 30,
     bio:
         'Psychology, science, and art are what helps me to learn the outside world and myself.',
-    imgUrl: 'assets/images/user6.jpg',
+    imgUrl: 'assets/images/user6.jp6g',
     location: 'Birmingham',
   ),
   User(
