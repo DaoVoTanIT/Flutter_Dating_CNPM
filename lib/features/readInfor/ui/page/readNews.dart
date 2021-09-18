90865b4315b57a55db01fbda43bda7ce30be09ca
DaoVoTanIT <daovotan3@gmail.com>
2021-09-18 21:20:27 +0700

    create chatPage screen

---
+++ lib/features/readInfor/ui/page/readNews.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReadNewsPage extends StatefulWidget {
+  const ReadNewsPage({Key? key}) : super(key: key);
+
+  @override
+  _ReadNewsPageState createState() => _ReadNewsPageState();
+}
+
+class _ReadNewsPageState extends State<ReadNewsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
