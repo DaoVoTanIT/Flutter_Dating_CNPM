d535fe80a994a067864fd9d8da72f15422caf1ad
DaoVoTanIT <daovotan3@gmail.com>
2021-09-18 21:17:05 +0700

    add route to tab

--- lib/routes/route_name.dart
+++ lib/routes/route_name.dart
@@ -2,6 +2,9 @@ class RouteName {
   static final String splashPage = 'SplashPage';
   static final String loginPage = 'LoginPage';
   static final String homePage = 'HomePage';
-  static final String chatPage = 'chatPage';
+  static final String chatPage = 'ChatPage';
   static final String profilePage = 'ProfilePage';
+  static final String curvedNavigationBarWidget =
+      'CurvedNavigationBarWidget'; //CurvedNavigationBarWidget
+
 }
--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dating_cdcnpm/features/chat/ui/page/chatPage.dart';
 import 'package:flutter_dating_cdcnpm/features/home/ui/page/homepage.dart';
+import 'package:flutter_dating_cdcnpm/features/home/ui/widget/tabBar.dart';
 import 'package:flutter_dating_cdcnpm/features/signin/ui/page/signin.dart';
 import 'package:flutter_dating_cdcnpm/features/splash/ui/page/splash.dart';
 
@@ -10,9 +12,14 @@ class Routers {
         return MaterialPageRoute(builder: (context) => SplashPage());
       case 'LoginPage':
         return MaterialPageRoute(builder: (context) => SignInPage());
+      case 'CurvedNavigationBarWidget':
+        return MaterialPageRoute(
+            builder: (context) => CurvedNavigationBarWidget());
       case 'HomePage':
         return MaterialPageRoute(builder: (context) => HomePage());
-
+      case 'ChatPage':
+        return MaterialPageRoute(
+            builder: (context) => ChatPage()); //CurvedNavigationBarWidget
       default:
         {
           return MaterialPageRoute(builder: (_) {
