d943ff3bb40918b26fdc02b76f278354cc37c01d
DaoVoTanIT <daovotan3@gmail.com>
2021-09-18 22:02:12 +0700

    add appBar in chatPage

--- lib/features/chat/ui/page/chatPage.dart
+++ lib/features/chat/ui/page/chatPage.dart
@@ -1,4 +1,8 @@
+import 'package:easy_gradient_text/easy_gradient_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dating_cdcnpm/style/styleAppBar.dart';
+import 'package:flutter_dating_cdcnpm/theme/color.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -11,6 +15,48 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: StyleAppBar(
+        backgroundColor: kBackgroudColor,
+        elevation: 0.0,
+        height: 60,
+        leading: InkWell(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsetsDirectional.only(start: 5),
+            child: ShaderMask(
+                child: Icon(
+                  FontAwesomeIcons.heartBroken,
+                  size: 42,
+                  color: Colors.redAccent,
+                ),
+                blendMode: BlendMode.srcATop,
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                          colors: [
+                        Colors.redAccent,
+                        Colors.deepPurpleAccent,
+                      ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          tileMode: TileMode.repeated)
+                      .createShader(bounds);
+                }),
+          ),
+        ),
+        title: GradientText(
+          text: 'Message',
+          colors: <Color>[Colors.redAccent, Colors.purpleAccent],
+          style: TextStyle(fontSize: 30.0),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.grey,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/notification');
+              })
+        ],
+      ),
       body: Text('hi'),
     );
   }
