2ce35a1898d62a0bfbcd5d7f0c2537055706e3c3
DaoVoTanIT <daovotan3@gmail.com>
2021-09-18 22:05:13 +0700

    add appBar in profile

--- lib/features/chat/ui/profile/ui/page/profile.dart
+++ lib/features/chat/ui/profile/ui/page/profile.dart
@@ -1,4 +1,8 @@
+import 'package:easy_gradient_text/easy_gradient_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dating_cdcnpm/style/styleAppBar.dart';
+import 'package:flutter_dating_cdcnpm/theme/color.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,6 +14,49 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: StyleAppBar(
+        backgroundColor: kBackgroudColor,
+        elevation: 0.0,
+        height: 60,
+        leading: InkWell(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsetsDirectional.only(start: 5),
+            child: ShaderMask(
+                child: Icon(
+                  FontAwesomeIcons.heartBroken,
+                  size: 42,
+                  color: Colors.redAccent,
+                ),
+                blendMode: BlendMode.srcATop,
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                          colors: [
+                        Colors.redAccent,
+                        Colors.deepPurpleAccent,
+                      ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          tileMode: TileMode.repeated)
+                      .createShader(bounds);
+                }),
+          ),
+        ),
+        title: GradientText(
+          text: 'Profile',
+          colors: <Color>[Colors.redAccent, Colors.purpleAccent],
+          style: TextStyle(fontSize: 30.0),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.settings),
+              color: Colors.grey,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/notification');
+              })
+        ],
+      ),
+    );
   }
 }
