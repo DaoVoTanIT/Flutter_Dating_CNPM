0234e862c8c9b34d86d097348b39a968f499353e
DaoVoTanIT <daovotan3@gmail.com>
2021-09-18 22:04:09 +0700

    add appBar in readNew

--- lib/features/readInfor/ui/page/readNews.dart
+++ lib/features/readInfor/ui/page/readNews.dart
@@ -1,4 +1,8 @@
+import 'package:easy_gradient_text/easy_gradient_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dating_cdcnpm/style/styleAppBar.dart';
+import 'package:flutter_dating_cdcnpm/theme/color.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ReadNewsPage extends StatefulWidget {
   const ReadNewsPage({Key? key}) : super(key: key);
@@ -10,6 +14,42 @@ class ReadNewsPage extends StatefulWidget {
 class _ReadNewsPageState extends State<ReadNewsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: StyleAppBar(
+        backgroundColor: kBackgroudColor,
+        elevation: 0.0,
+        height: 60,
+        leading: InkWell(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsetsDirectional.only(start: 5),
+            child: ShaderMask(
+                child: Icon(
+                  FontAwesomeIcons.heartBroken,
+                  size: 42,
+                  color: Colors.redAccent,
+                ),
+                blendMode: BlendMode.srcATop,
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                          colors: [
+                        Colors.redAccent,
+                        Colors.deepPurpleAccent,
+                      ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          tileMode: TileMode.repeated)
+                      .createShader(bounds);
+                }),
+          ),
+        ),
+        title: GradientText(
+          text: 'News',
+          colors: <Color>[Colors.redAccent, Colors.purpleAccent],
+          style: TextStyle(fontSize: 30.0),
+        ),
+        actions: [],
+      ),
+    );
   }
 }
