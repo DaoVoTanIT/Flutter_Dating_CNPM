5bc55c5019d891dfb05b6753502cf5997b9a2111
DaoVoTanIT <daovotan3@gmail.com>
2021-09-19 17:42:54 +0700

    create data of chat

---
+++ lib/features/chat/resource/chat.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_dating_cdcnpm/features/chat/model/chat.dart';
+
+List chatsData = [
+  Chat(
+    name: "Đào Tấn",
+    lastMessage: "Do you have update...",
+    image: "assets/images/dao.jpeg",
+    time: "5d ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Lauren Turner",
+    lastMessage: "Hope you are doing well...",
+    image: "assets/images/user1.jpg",
+    time: "3m ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Chiến Ngô",
+    lastMessage: "Chiến đã đi chơi rồi",
+    image: "assets/images/chien.jpeg",
+    time: "8m ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Nguyễn Thu Hương",
+    lastMessage: "Hế lô",
+    image: "assets/images/huong.jpeg",
+    time: "5d ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Nguyễn Thị Khánh Vân",
+    lastMessage: "Thanks",
+    image: "assets/images/khanhvan.jpeg",
+    time: "6d ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Lori Perez",
+    lastMessage: "Hope you are doing well...",
+    image: "assets/images/user2.jpg",
+    time: "3m ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Esther Howard",
+    lastMessage: "Hello Abdullah! I am...",
+    image: "assets/images/user4.jpg",
+    time: "8m ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Ralph Edwards",
+    lastMessage: "Do you have update...",
+    image: "assets/images/user6.jpeg",
+    time: "5d ago",
+    isActive: false,
+  ),
+];
