ab36f634cdbdcdbfea278fe7411872584b6f9da2
DaoVoTanIT <daovotan3@gmail.com>
2021-09-25 20:59:35 +0700

    create signIn screen

--- lib/features/signin/ui/page/signin.dart
+++ lib/features/signin/ui/page/signin.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dating_cdcnpm/features/home/resources/data.dart';
+import 'package:flutter_dating_cdcnpm/features/signin/ui/widget/header_widget.dart';
 import 'package:flutter_dating_cdcnpm/routes/route_name.dart';
+import 'package:flutter_dating_cdcnpm/style/theme_helper.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -10,22 +12,107 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  double _headerHeight = 250;
+  Key _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Sign In'),
-            OutlinedButton(
-              //style: outlineButtonStyle,
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, RouteName.curvedNavigationBarWidget);
-              },
-              child: Text(' OutlineButton'),
-            )
+            Container(
+              height: _headerHeight,
+              child: HeaderWidget(_headerHeight, true, Icons.login_rounded),
+            ),
+            SafeArea(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: EdgeInsets.fromLTRB(
+                      20, 10, 20, 10), // This will be the login form
+                  child: Column(
+                    children: [
+                      Text(
+                        'Xin chào',
+                        style: TextStyle(
+                            fontSize: 60, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Đăng nhập vào tài khoản của bạn',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(height: 30.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Container(
+                                child: TextField(
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Tài khoản', 'Hãy nhập tài khoản'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              SizedBox(height: 30.0),
+                              Container(
+                                child: TextField(
+                                  obscureText: true,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Mật khẩu', 'Hãy nhập mật khẩu'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              SizedBox(height: 15.0),
+                              Container(
+                                decoration:
+                                    ThemeHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemeHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                    child: Text(
+                                      'Đăng nhập'.toUpperCase(),
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    //After successful login we will redirect to profile page. Let's create profile page now
+                                    Navigator.pushNamed(context,
+                                        RouteName.curvedNavigationBarWidget);
+                                  },
+                                ),
+                              ),
+                            ],
+                          )),
+                      SizedBox(height: 30),
+                      Row(
+                        children: [
+                          Text(
+                            'Bạn chưa có tài khoản?',
+                            style: TextStyle(fontSize: 17.0),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                  context, RouteName.registrationPage);
+                            },
+                            child: Text(
+                              ' Đăng kí ngay',
+                              style:
+                                  TextStyle(fontSize: 17.0, color: Colors.blue),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            ),
           ],
         ),
       ),
