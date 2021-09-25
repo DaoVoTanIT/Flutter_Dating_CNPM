f9420c67819b234e3b863b8d2ed03ee232947c5b
DaoVoTanIT <daovotan3@gmail.com>
2021-09-25 20:57:49 +0700

    change color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         // appBarTheme: AppBarTheme(
         //     color: kPrimaryColorVariant,
         //     brightness: Brightness.light,
